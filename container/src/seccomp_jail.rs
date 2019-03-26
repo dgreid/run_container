2091b24afdc6952ffe1fe3cd17cb26c611597aa8
Dylan Reid <dgreid@chromium.org>
2019-03-26 12:12:31 -0700

    seccomp_tests_pass

--- container/src/seccomp_jail.rs
+++ container/src/seccomp_jail.rs
@@ -52,7 +52,7 @@ fn action_from_string(act_str: &str) -> Result<Action, Error> {
     match act_str {
         "SCMP_ACT_KILL" => Ok(Action::Kill),
         "SCMP_ACT_TRAP" => Ok(Action::Trap),
-        "SCMP_ACT_ERRNO" => Ok(Action::Errno(-1)),
+        "SCMP_ACT_ERRNO" => Ok(Action::Errno(1)),
         "SCMP_ACT_TRACE" => Ok(Action::Trace(1)), // TODO
         "SCMP_ACT_ALLOW" => Ok(Action::Allow),
         _ => Err(Error::InvalidAction(act_str.to_string())),
@@ -76,19 +76,20 @@ impl SeccompConfig {
     pub fn new(default_action: &str) -> Result<SeccompConfig, Error> {
         let default_action = action_from_string(default_action)?;
         Ok(SeccompConfig {
-               default_action: default_action,
-               rules: HashMap::new(),
-           })
+            default_action: default_action,
+            rules: HashMap::new(),
+        })
     }
 
-    pub fn add_rule(&mut self,
-                    syscall_name: &str,
-                    action: &str,
-                    arg_index: Option<u32>,
-                    val: Option<u64>,
-                    val2: Option<u64>,
-                    op: Option<&str>)
-                    -> Result<(), Error> {
+    pub fn add_rule(
+        &mut self,
+        syscall_name: &str,
+        action: &str,
+        arg_index: Option<u32>,
+        val: Option<u64>,
+        val2: Option<u64>,
+        op: Option<&str>,
+    ) -> Result<(), Error> {
         let hash_key = SyscallRule {
             name: syscall_name.to_string(),
             action: action.to_string(),
@@ -96,11 +97,11 @@ impl SeccompConfig {
         let ops = self.rules.entry(hash_key).or_insert_with(Vec::new);
         if let (Some(op), Some(arg_index)) = (op, arg_index) {
             ops.push(seccomp_sys::scmp_arg_cmp {
-                         arg: arg_index,
-                         op: op_from_string(op)?,
-                         datum_a: val.unwrap_or(0),
-                         datum_b: val2.unwrap_or(0),
-                     });
+                arg: arg_index,
+                op: op_from_string(op)?,
+                datum_a: val.unwrap_or(0),
+                datum_b: val2.unwrap_or(0),
+            });
         }
         Ok(())
     }
@@ -114,14 +115,16 @@ impl SeccompJail {
                 return Err(Error::SeccompInitFail);
             }
             for (key, ops) in &config.rules {
-                let syscall_number = syscall_defines::from_name(&key.name)
-                    .map_err(|_| Error::InvalidSyscall)?;
+                let syscall_number =
+                    syscall_defines::from_name(&key.name).map_err(|_| Error::InvalidSyscall)?;
                 let action = action_number(&action_from_string(&key.action)?);
-                let ret = seccomp_sys::seccomp_rule_add_array(context,
-                                                              action,
-                                                              syscall_number as i32,
-                                                              ops.len() as u32,
-                                                              ops.as_slice().as_ptr());
+                let ret = seccomp_sys::seccomp_rule_add_array(
+                    context,
+                    action,
+                    syscall_number as i32,
+                    ops.len() as u32,
+                    ops.as_slice().as_ptr(),
+                );
                 if ret != 0 {
                     seccomp_sys::seccomp_release(context);
                     return Err(Error::SeccompRuleAddFail);
@@ -163,13 +166,15 @@ mod test {
             }
         };
         assert!(config
-                    .add_rule("getuid",
-                              "SCMP_ACT_ERRNO",
-                              Some(0),
-                              Some(0),
-                              None,
-                              Some("SCMP_CMP_GE"))
-                    .is_ok());
+            .add_rule(
+                "getuid",
+                "SCMP_ACT_ERRNO",
+                Some(0),
+                Some(0),
+                None,
+                Some("SCMP_CMP_GE")
+            )
+            .is_ok());
         let jail = match SeccompJail::new(&config) {
             Ok(j) => j,
             Err(e) => {
@@ -195,24 +200,24 @@ mod test {
             }
         };
         assert!(config
-                    .add_rule("getuid", "SCMP_ACT_ALLOW", None, None, None, None)
-                    .is_ok());
+            .add_rule("getuid", "SCMP_ACT_ALLOW", None, None, None, None)
+            .is_ok());
         assert!(config
-                    .add_rule("futex", "SCMP_ACT_ALLOW", None, None, None, None)
-                    .is_ok());
+            .add_rule("futex", "SCMP_ACT_ALLOW", None, None, None, None)
+            .is_ok());
         assert!(config
-                    .add_rule("exit", "SCMP_ACT_ALLOW", None, None, None, None)
-                    .is_ok());
+            .add_rule("exit", "SCMP_ACT_ALLOW", None, None, None, None)
+            .is_ok());
         assert!(config
-                    .add_rule("exit_group", "SCMP_ACT_ALLOW", None, None, None, None)
-                    .is_ok());
+            .add_rule("exit_group", "SCMP_ACT_ALLOW", None, None, None, None)
+            .is_ok());
         assert!(config
-                    .add_rule("rt_sigreturn", "SCMP_ACT_ALLOW", None, None, None, None)
-                    .is_ok());
+            .add_rule("rt_sigreturn", "SCMP_ACT_ALLOW", None, None, None, None)
+            .is_ok());
         let jail = match SeccompJail::new(&config) {
             Ok(j) => j,
             Err(e) => {
-                println!("{:?}", e);
+                println!("{:?} {:?}", e, unsafe { *libc::__errno_location() });
                 assert!(false);
                 return;
             }
